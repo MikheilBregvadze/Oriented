b925c6e9d87e49e6879373dfa45020e405bfb8a8
Mikheil Bregvadze <m.bregvadze@Oakland.loc>
2018-12-07 17:47:40 +0400

    add orienterd

---
+++ src/fleet-data.js
@@ -0,0 +1,64 @@
+export let fleet = [
+    {
+        license: 'ABC123',
+        type: 'drone',
+        model: 'Amazon 1250',
+        airTimeHours: '6050',
+        base: 'New York',
+        latLong: '40.777443 - 73.54334'
+    },
+    {
+        license: 'ABC123',
+        type: 'drone',
+        model: 'Amazon 1250',
+        airTimeHours: '6050',
+        base: 'New York',
+        latLong: '40.777443 - 73.54334'
+    },
+    {
+        license: 'ABC123',
+        type: 'drone',
+        model: 'Amazon 1250',
+        airTimeHours: '6050',
+        base: 'New York',
+        latLong: '40.777443 - 73.54334'
+    },
+    {
+        license: 'ABC123',
+        type: 'drone',
+        model: 'Amazon 1250',
+        airTimeHours: '6050',
+        base: 'New York',
+        latLong: '40.777443 - 73.54334'
+    },
+    {
+        license: 'ABC123',
+        type: 'car',
+        make: 'Tesla',
+        model: 'Quick Transport',
+        miles: '12358',
+        latLong: '40.777443 - 73.54334'
+    },
+    {
+        license: 'ABC123',
+        type: 'car',
+        make: 'Tesla',
+        model: 'Quick Transport',
+        miles: '12358',
+        latLong: '40.777443 - 73.54334'
+    },{
+        license: 'ABC123',
+        type: 'car',
+        make: 'Tesla',
+        model: 'Quick Transport',
+        miles: '12358',
+        latLong: '40.777443 - 73.54334'
+    },{
+        license: 'ABC123',
+        type: 'car',
+        make: 'Tesla',
+        model: 'Quick Transport',
+        miles: '12358',
+        latLong: '40.777443 - 73.54334'
+    },
+]
