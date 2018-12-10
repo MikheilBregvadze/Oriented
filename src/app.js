0c20dd82359bfcc8c96625303079d6a50511976c
Mikheil Bregvadze <m.bregvadze@Oakland.loc>
2018-12-10 12:44:40 +0400

    merge

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import {Car} from './classes/car.js';
 import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js';
-// import {fleetDataService} from '../services/fleet-data-services.js';
-import { FleetDataService } from './services/fleet-data-service.js';
-
+import {FleetDataService} from './services/fleet-data-service.js';
 
 let dataService = new FleetDataService();
 
 dataService.loadData(fleet);
 
-console.log(dataService.drones);
+for(let e of dataService.errors)
+    console.log(e.message);
--- src/fleet-data.js
+++ src/fleet-data.js
@@ -2,63 +2,65 @@ export let fleet = [
     {
         license: 'ABC123',
         type: 'drone',
-        model: 'Amazon 1250',
+        model: 'Amazon 13250',
         airTimeHours: '6050',
-        base: 'New York',
+        base: 'New Yorkq',
         latLong: '40.777443 - 73.54334'
     },
     {
-        license: 'ABC123',
+        license: 'ABD123',
+        type: 'drone',
+        model: 'Amazon 12250',
+        airTimeHours: '6050',
+        base: 'Nqew York',
+        latLong: '40.777443 - 73.54334'
+    },
+    {
+        license: 'ABF123',
+        type: 'drone',
+        model: 'Amazon 11250',
+        airTimeHours: '6050',
+        base: 'Neew York',
+        latLong: '40.777443 - 73.54334'
+    },
+    {
+        license: 'ABG123',
         type: 'drone',
         model: 'Amazon 1250',
         airTimeHours: '6050',
-        base: 'New York',
-        latLong: '40.777443 - 73.54334'
+        base: 'New Yorke',
+        latLong: '40.7717443 - 73.543134'
     },
     {
-        license: 'ABC123',
-        type: 'drone',
-        model: 'Amazon 1250',
-        airTimeHours: '6050',
-        base: 'New York',
-        latLong: '40.777443 - 73.54334'
+        license: 'ABH123',
+        type: 'car',
+        make: 'Tesla',
+        model: 'Quick Transportq',
+        miles: '12358',
+        latLong: '40.7772443 - 73.54334'
     },
     {
-        license: 'ABC123',
-        type: 'drone',
-        model: 'Amazon 1250',
-        airTimeHours: '6050',
-        base: 'New York',
-        latLong: '40.777443 - 73.54334'
+        license: 'ABJ123',
+        type: 'car',
+        make: 'Teslaa',
+        model: 'Quick Transporta',
+        miles: '12358',
+        latLong: '40.7774413 - 73.54334'
     },
     {
-        license: 'ABC123',
+        license: 'ABK123',
         type: 'car',
-        make: 'Tesla',
+        make: 'Teslas',
         model: 'Quick Transport',
         miles: '12358',
-        latLong: '40.777443 - 73.54334'
+        latLong: '40.777443 - 73.154334'
     },
     {
-        license: 'ABC123',
+        license: 'ABL123',
         type: 'car',
-        make: 'Tesla',
-        model: 'Quick Transport',
+        make: 'Teslad',
+        model: 'Quick Transportx',
         miles: '12358',
-        latLong: '40.777443 - 73.54334'
-    },{
-        license: 'ABC123',
-        type: 'car',
-        make: 'Tesla',
-        model: 'Quick Transport',
-        miles: '12358',
-        latLong: '40.777443 - 73.54334'
-    },{
-        license: 'ABC123',
-        type: 'car',
-        make: 'Tesla',
-        model: 'Quick Transport',
-        miles: '12358',
-        latLong: '40.777443 - 73.54334'
+        latLong: '40.777443 - 73.541334'
     },
 ]
